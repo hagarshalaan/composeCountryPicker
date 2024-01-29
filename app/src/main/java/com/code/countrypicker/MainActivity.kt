7bf1980b4c4969ddb33cbdd32fbe2b77a400dfbe
Hagar <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>
2024-01-29 02:59:56 +0200

    change the view to fill max size

--- app/src/main/java/com/code/countrypicker/MainActivity.kt
+++ app/src/main/java/com/code/countrypicker/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.code.composecountrypicker.utils.codeCountryView
 import com.code.countrypicker.ui.theme.CountryPickerTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +24,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    codeCountryView()
+
                 }
             }
         }
@@ -42,5 +45,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     CountryPickerTheme {
         Greeting("Android")
+        codeCountryView()
     }
 }
--- composeCountryPicker/src/main/java/com/code/composecountrypicker/utils/Countries.kt
+++ composeCountryPicker/src/main/java/com/code/composecountrypicker/utils/Countries.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -330,9 +331,8 @@ fun CountryCodePicker(
     val filteredCountries = remember { mutableStateListOf<Country>() }
 
     Column(
-        modifier
+        modifier.fillMaxSize()
             .padding(paddingHigher())
-            .fillMaxWidth()
     ) {
         if (displaySearch) {
             searchBar(onValueChange = { query ->
